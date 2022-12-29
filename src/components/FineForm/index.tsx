94b6f121fc56c103929dab49e53a25daa80712bd
helipilot50 <helipilot50@gmail.com>
2022-12-29 19:52:26 +0100

    formik and FluentUI is working

--- src/components/FineForm/index.tsx
+++ src/components/FineForm/index.tsx
@@ -1,6 +1,6 @@
-import { DefaultButton, PrimaryButton, TextField } from '@fluentui/react';
-import React from 'react';
-import { Formik, Field, Form, FormikHelpers, FieldProps } from 'formik';
+import { DefaultButton, PrimaryButton, TextField, Text } from '@fluentui/react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 interface Values {
   firstName: string;
@@ -8,41 +8,56 @@ interface Values {
   email: string;
 }
 
+const validationSchema = yup.object({
+  email: yup
+    .string()
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+});
+
 function FineForm() {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <div>
-      <h1>Signup</h1>
-      <Formik
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          email: '',
-        }}
-        onSubmit={(
-          values: Values,
-          { setSubmitting }: FormikHelpers<Values>
-        ) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 500);
-        }}
-      >
-        <Form>
-          <Field id="firstName" name="firstName" placeholder="John" as={TextField} />
-          <Field id="lastName" name="lastName" placeholder="Doe" as={TextField} />
-          <Field
-            id="email"
-            name="email"
-            placeholder="john@acme.com"
-            type="email"
-            as={TextField}
-          />
+      <Text variant='xLarge'>Details</Text>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          id="email"
+          name="email"
+          label="Email"
+          placeholder='example@someplace.com'
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          errorMessage={(formik.touched.email && Boolean(formik.errors.email)) ? formik.errors.email : undefined}
+        />
+        <TextField
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          placeholder='your password here'
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          errorMessage={(formik.touched.password && Boolean(formik.errors.password)) ? formik.errors.password : undefined}
+        />
 
-          <PrimaryButton type="submit" text='Save' />
-          <DefaultButton text='Cancel' />
-        </Form>
-      </Formik>
+        <PrimaryButton type="submit" text='Save' />
+        <DefaultButton text='Cancel' />
+      </form>
+
     </div>
   );
 }
